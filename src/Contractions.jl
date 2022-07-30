8d21f1a6024caf82f437807b45d616c7840d518f
agandotra611 <adityag@princeton.edu>
2022-07-30 12:08:44 -0400

    Completeld contraction

--- src/Contractions.jl
+++ src/Contractions.jl
@@ -1,21 +1,18 @@
 module Contractions
 
 mutable struct diagramNode
-    root::AbstractArray{Tuple{Int, Int}, N}
+    root::Tuple{Int, Int}
     val::Tuple{Int, Int}
     right::diagramNode
     left::diagramNode
 
-    function getDiagram(node::diagramNode)
-        partitions = [] #Get all partitions of node.val
-        diagram = !append(partitions, node.root)
+    function diagramNode(root, val)
+        (val[1] > 0) ? left = diagramNode(root, (Int(val[1]-1), Int(val[2]))) : left = 0
+        (val[2] > 0) ? right = diagramNode(root, (Int(val[1]), Int(val[2]-1))) : right = 0
+        new(root, val, left, right)
         
     end
 
 end
-
-#Write recursive algorithm that does the partitions at each level 
-
-
 
 end
