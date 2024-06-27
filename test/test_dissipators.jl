ae88848e1b2c30c96fb06da60df54eeecbfdd535
Leon Bello <lionbello@gmail.com>
2024-06-26 20:45:00 -0400

    Fixed some tests to be compatible with current Julia

--- test/test_dissipators.jl
+++ test/test_dissipators.jl
@@ -35,8 +35,8 @@ using QuantumGraining
     # ((a*σp), (a*σm)): 2ωc
     @test isequal(ω_eff_2[((a*σp), (a*σm))], 2*ωc)
     @test issetequal(γ_eff_2[((a*σp), (a*σm))].exponents, [(ωa + ωc)^2 + (-ωa + ωc)^2 + 2(ωa + ωc)*(-ωa + ωc), (ωa + ωc)^2 + (-ωa + ωc)^2])
-    @test simplify(γ_eff_2[((a*σp), (a*σm))].prefacs[1] .- -1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))) == 0
-    @test simplify(γ_eff_2[((a*σp), (a*σm))].prefacs[2] .- 1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a*σp), (a*σm))].prefacs[1] .- -1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))), 0)
+    @test isequal(simplify(γ_eff_2[((a*σp), (a*σm))].prefacs[2] .- 1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))), 0)
     @test length(γ_eff_2[((a*σp), (a*σm))].exponents) == 2
     @test issetequal(γ_eff_2[((a*σp), (a*σm))].polys, [[1],[1]])
 
@@ -44,48 +44,48 @@ using QuantumGraining
     @test isequal(ω_eff_2[((a'*σm), (a*σm))], 2*ωa)
     @test issetequal(γ_eff_2[((a'*σm), (a*σm))].exponents, [(ωa + ωc)^2 + (ωa - ωc)^2 + 2(ωa + ωc)*(ωa - ωc), (ωa + ωc)^2 + (ωa - ωc)^2])
     @test length(γ_eff_2[((a'*σm), (a*σm))].exponents) == 2
-    @test simplify(γ_eff_2[((a'*σm), (a*σm))].prefacs[1] .- 1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))) == 0
-    @test simplify(γ_eff_2[((a'*σm), (a*σm))].prefacs[2] .- -1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a'*σm), (a*σm))].prefacs[1] .- 1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))), 0)
+    @test isequal(simplify(γ_eff_2[((a'*σm), (a*σm))].prefacs[2] .- -1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))), 0)
     @test issetequal(γ_eff_2[((a'*σm), (a*σm))].polys, [[1],[1]])
 
     # ((a'*σp), (a'*σp)): -2ωa - 2ωc
     @test isequal(ω_eff_2[((a'*σp), (a'*σp))], -2ωa - 2ωc)
     @test issetequal(γ_eff_2[((a'*σp), (a'*σp))].exponents, [4((-ωa - ωc)^2), 2((-ωa - ωc)^2)])
     @test length(γ_eff_2[((a'*σp), (a'*σp))].exponents) == 2
-    @test simplify(γ_eff_2[((a'*σp), (a'*σp))].prefacs[1] .- -1im*(g^2//2)*1/(ωa + ωc)) == 0
-    @test simplify(γ_eff_2[((a'*σp), (a'*σp))].prefacs[2] .- 1im*(g^2//2)*1/(ωa + ωc)) == 0
+    @test isequal(simplify(γ_eff_2[((a'*σp), (a'*σp))].prefacs[1] .- -1im*(g^2//2)*1/(ωa + ωc)), 0)
+    @test isequal((γ_eff_2[((a'*σp), (a'*σp))].prefacs[2] .- 1im*(g^2//2)*1/(ωa + ωc)), 0)
     @test issetequal(γ_eff_2[((a'*σp), (a'*σp))].polys, [[1],[1]])
 
     # ((a*σp), (a'*σp)): -2ωa
     @test isequal(ω_eff_2[((a*σp), (a'*σp))], -2ωa)
     @test issetequal(γ_eff_2[((a*σp), (a'*σp))].exponents, [(-ωa - ωc)^2 + (-ωa + ωc)^2 + 2(-ωa + ωc)*(-ωa - ωc), (-ωa - ωc)^2 + (-ωa + ωc)^2])
     @test length(γ_eff_2[((a*σp), (a'*σp))].exponents) == 2
-    @test simplify(γ_eff_2[((a*σp), (a'*σp))].prefacs[1] .- -1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))) == 0
-    @test simplify(γ_eff_2[((a*σp), (a'*σp))].prefacs[2] .- 1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a*σp), (a'*σp))].prefacs[1] .- -1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))), 0)
+    @test isequal(simplify(γ_eff_2[((a*σp), (a'*σp))].prefacs[2] .- 1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))), 0)
     @test issetequal(γ_eff_2[((a*σp), (a'*σp))].polys, [[1],[1]])
 
     # ((a'*σm), (a'*σp)): -2ωc
     @test isequal(ω_eff_2[((a'*σm), (a'*σp))], -2ωc)
     @test issetequal(γ_eff_2[((a'*σm), (a'*σp))].exponents, [(ωa - ωc)^2 + (-ωa - ωc)^2 + 2(ωa - ωc)*(-ωa - ωc), (ωa - ωc)^2 + (-ωa - ωc)^2])
     @test length(γ_eff_2[((a'*σm), (a'*σp))].exponents) == 2
-    @test simplify(γ_eff_2[((a'*σm), (a'*σp))].prefacs[1] .- 1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))) == 0
-    @test simplify(γ_eff_2[((a'*σm), (a'*σp))].prefacs[2] .- -1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a'*σm), (a'*σp))].prefacs[1] .- 1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))), 0)
+    @test isequal(simplify(γ_eff_2[((a'*σm), (a'*σp))].prefacs[2] .- -1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))), 0)
     @test issetequal(γ_eff_2[((a'*σm), (a'*σp))].polys, [[1],[1]])
 
     # ((a*σm), (a*σp)): 2ωc
     @test isequal(ω_eff_2[((a*σm), (a*σp))], 2ωc)
     @test issetequal(γ_eff_2[((a*σm), (a*σp))].exponents, [(ωa + ωc)^2 + (-ωa + ωc)^2 + 2(ωa + ωc)*(-ωa + ωc), (ωa + ωc)^2 + (-ωa + ωc)^2])
     @test length(γ_eff_2[((a*σm), (a*σp))].exponents) == 2
-    @test simplify(γ_eff_2[((a*σm), (a*σp))].prefacs[1] .- -1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))) == 0
-    @test simplify(γ_eff_2[((a*σm), (a*σp))].prefacs[2] .- 1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a*σm), (a*σp))].prefacs[1] .- -1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))), 0)
+    @test isequal(simplify(γ_eff_2[((a*σm), (a*σp))].prefacs[2] .- 1im*(g^2//2)*ωc/((ωa + ωc)*(ωa - ωc))), 0)
     @test issetequal(γ_eff_2[((a*σm), (a*σp))].polys, [[1],[1]])
 
     # ((a'*σp), (a*σp)): -2ωa
     @test isequal(ω_eff_2[((a'*σp), (a*σp))], -2ωa)
     @test issetequal(γ_eff_2[((a'*σp), (a*σp))].exponents, [(-ωa - ωc)^2 + (-ωa + ωc)^2 + 2(-ωa + ωc)*(-ωa - ωc), (-ωa - ωc)^2 + (-ωa + ωc)^2])
     @test length(γ_eff_2[((a'*σp), (a*σp))].exponents) == 2
-    @test simplify(γ_eff_2[((a'*σp), (a*σp))].prefacs[1] .- -1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))) == 0
-    @test simplify(γ_eff_2[((a'*σp), (a*σp))].prefacs[2] .- 1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a'*σp), (a*σp))].prefacs[1] .- -1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))), 0)
+    @test isequal(simplify(γ_eff_2[((a'*σp), (a*σp))].prefacs[2] .- 1im*(g^2//2)*ωa/((ωa + ωc)*(ωa - ωc))), 0)
     @test issetequal(γ_eff_2[((a'*σp), (a*σp))].polys, [[1],[1]])
 
 
@@ -93,31 +93,31 @@ using QuantumGraining
     @test isequal(ω_eff_2[((a*σp), (a*σp))], -2ωa + 2ωc)
     @test issetequal(γ_eff_2[((a*σp), (a*σp))].exponents, [4((-ωa + ωc)^2), 2((-ωa + ωc)^2)])
     @test length(γ_eff_2[((a*σp), (a*σp))].exponents) == 2
-    @test simplify(γ_eff_2[((a*σp), (a*σp))].prefacs[1] .- -1im*(g^2//2)*1/(ωa - ωc)) == 0
-    @test simplify(γ_eff_2[((a*σp), (a*σp))].prefacs[2] .- 1im*((g^2//2)*1/(ωa - ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a*σp), (a*σp))].prefacs[1] .- -1im*(g^2//2)*1/(ωa - ωc)), 0)
+    @test isequal(simplify(γ_eff_2[((a*σp), (a*σp))].prefacs[2] .- 1im*((g^2//2)*1/(ωa - ωc))), 0)
     @test issetequal(γ_eff_2[((a*σp), (a*σp))].polys, [[1],[1]])
 
     # ((a*σm), (a'*σm)): 2ωa
     @test isequal(ω_eff_2[((a*σm), (a'*σm))], 2ωa)
     @test issetequal(γ_eff_2[((a*σm), (a'*σm))].exponents, [(ωa + ωc)^2 + (ωa - ωc)^2 + 2(ωa + ωc)*(ωa - ωc), (ωa + ωc)^2 + (ωa - ωc)^2])
     @test length(γ_eff_2[((a*σm), (a'*σm))].exponents) == 2
-    @test simplify(γ_eff_2[((a*σm), (a'*σm))].prefacs[1] .- 1im*(g^2//2)*ωa/((ωa - ωc)*(ωa + ωc))) == 0
-    @test simplify(γ_eff_2[((a*σm), (a'*σm))].prefacs[2] .- -1im*(g^2//2)*ωa/((ωa - ωc)*(ωa + ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a*σm), (a'*σm))].prefacs[1] .- 1im*(g^2//2)*ωa/((ωa - ωc)*(ωa + ωc))), 0)
+    @test isequal(simplify(γ_eff_2[((a*σm), (a'*σm))].prefacs[2] .- -1im*(g^2//2)*ωa/((ωa - ωc)*(ωa + ωc))), 0)
     @test issetequal(γ_eff_2[((a*σm), (a'*σm))].polys, [[1],[1]])
 
     # ((a'*σp), (a'*σm)): -2ωc
     @test isequal(ω_eff_2[((a'*σp), (a'*σm))], -2ωc)
     @test issetequal(γ_eff_2[((a'*σp), (a'*σm))].exponents, [(ωa - ωc)^2 + (-ωa - ωc)^2 + 2(ωa - ωc)*(-ωa - ωc), (ωa - ωc)^2 + (-ωa - ωc)^2])
     @test length(γ_eff_2[((a'*σp), (a'*σm))].exponents) == 2
-    @test simplify(γ_eff_2[((a'*σp), (a'*σm))].prefacs[1] .- 1im*(g^2//2)*ωc/((ωa - ωc)*(ωa + ωc))) == 0
-    @test simplify(γ_eff_2[((a'*σp), (a'*σm))].prefacs[2] .- -1im*(g^2//2)*ωc/((ωa - ωc)*(ωa + ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a'*σp), (a'*σm))].prefacs[1] .- 1im*(g^2//2)*ωc/((ωa - ωc)*(ωa + ωc))), 0)
+    @test isequal(simplify(γ_eff_2[((a'*σp), (a'*σm))].prefacs[2] .- -1im*(g^2//2)*ωc/((ωa - ωc)*(ωa + ωc))), 0)
     @test issetequal(γ_eff_2[((a'*σp), (a'*σm))].polys, [[1],[1]])
 
     # ((a'*σm), (a'*σm)): 2ωa−2ωc
     @test isequal(ω_eff_2[((a'*σm), (a'*σm))], 2ωa-2ωc)
     @test issetequal(γ_eff_2[((a'*σm), (a'*σm))].exponents, [4((ωa - ωc)^2), 2((ωa - ωc)^2)])
     @test length(γ_eff_2[((a'*σm), (a'*σm))].exponents) == 2
-    @test simplify(γ_eff_2[((a'*σm), (a'*σm))].prefacs[1] .- 1im*(g^2//2)*1/(ωa - ωc)) == 0
-    @test simplify(γ_eff_2[((a'*σm), (a'*σm))].prefacs[2] .- -1im*((g^2//2)*1/(ωa - ωc))) == 0
+    @test isequal(simplify(γ_eff_2[((a'*σm), (a'*σm))].prefacs[1] .- 1im*(g^2//2)*1/(ωa - ωc)), 0)
+    @test isequal(simplify(γ_eff_2[((a'*σm), (a'*σm))].prefacs[2] .- -1im*((g^2//2)*1/(ωa - ωc))), 0)
     @test issetequal(γ_eff_2[((a'*σm), (a'*σm))].polys, [[1],[1]])
 end
